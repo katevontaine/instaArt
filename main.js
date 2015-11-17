53a45a92d261a30f032343e04bdebdeab4483860
Kate Wilson <KateWilson@Kates-MacBook-Pro.local>
2015-11-17 09:28:55 -0500

    will add pic

--- main.js
+++ main.js
@@ -14,20 +14,22 @@ $(document).ready(function () {
     alert('it submit');
     event.preventDefault();
 
+    var myPhotoCollection = new ItemCollection();
+    var myPhotos = new ItemModel({imageUrl: picLink, title: picTitle, likes: '0'});
+    myPhotoCollection.fetch().then(function (collectionData) {
+      console.log(myPhotoCollection);
+      console.log(myPhotos);
+      myPhotos.save({imageUrl: picLink, title: picTitle,});
+    // myPhotoCollection.models.picTitle.save();
+      // myPhotoCollection.models.picLink.save();
+    });
+
     console.log($(this).siblings('input[name="Image"]').val());
     var picTitle = ($(this).siblings('input[name="Title"]').val());
     var picLink = ($(this).siblings('input[name="Image"]').val());
     $('.main').append(picTitle + "<img src='" + picLink + "'>");
+    $('.main').html(myPhotoCollection);
 
   })
-
-var myPhotoCollection = new ItemCollection();
-var myPhotos = new ItemModel({imageUrl: picLink, title:picTitle, likes: '0'});
-myPhotoCollection.fetch().then(function (collectionData) {
-  console.log(myPhotoCollection);
-  // myPhotoCollection.models.picTitle.save();
-  // myPhotoCollection.models.picLink.save();
-});
-
 
 });
